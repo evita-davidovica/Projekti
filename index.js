5849df9aeb068de9fb81a9b13a98b3b37374ad6a
Evita Davidoviča <evita.elizabete22@gmail.com>
2026-02-03 14:10:04 +0200

    Labots fails index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("Sveiki!");
+
+const utils = require('./Module5/src/utils/utils.js');
+const higherOrder = require('./Module5/src/utils/highter-order.js');
+const validators = require('./Module5/src/validators/validators.js');
+const cli = require('./Module5/src/cli/calculator-refactored.js');
+
+console.log('Projekts darbojas!');

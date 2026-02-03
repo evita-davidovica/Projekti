7f1eeee69751c34b7167b6368965167671829d95
Evita Davidoviča <evita.elizabete22@gmail.com>
2026-02-03 14:26:19 +0200

    Clean up: remove old/duplicate files

--- src/hello world plus.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello, World!");
-console.log("Pašreizējais datums un laiks: " + new Date().toLocaleString());
-console.log("Node.js versija: " + process.version);
-console.log("Operētājsistēmas platforma: " + process.platform);

176b8f6ea3bf41f583d050a03668c1d3c83f9bb4
Evita Davidoviča <evita.elizabete22@gmail.com>
2026-02-03 14:19:56 +0200

    Labota drukas kluda index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("Sveiki!");
 
 const utils = require('./Module5/src/utils/utils.js');
-const higherOrder = require('./Module5/src/utils/highter-order.js');
+const higherOrder = require('./Module5/src/utils/higher-order.js');
 const validators = require('./Module5/src/validators/validators.js');
 const cli = require('./Module5/src/cli/calculator-refactored.js');
 

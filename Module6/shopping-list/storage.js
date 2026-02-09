0c43b547d6ac1d0a4756001bf112509948a65a1f
Evita Davidoviča <evita.elizabete22@gmail.com>
2026-02-09 14:44:04 +0200

    Parlabots storage.js fails

--- Module6/shopping-list/storage.js
+++ Module6/shopping-list/storage.js
@@ -37,5 +37,33 @@ function saveList(items) { // Saglabā sarakstu failā
     fs.writeFileSync(PATH, JSON.stringify(normalized, null, 2)); // null, 2 - formatē JSON ar atkāpēm, lai būtu vieglāk lasāms
 }
 
-module.exports = { loadList, saveList };
+const PRICES_PATH = 'prices.json';
+
+function loadPrices() {
+    try {
+        const data = fs.readFileSync(PRICES_PATH, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        return {};
+    }
+}
+
+function savePrices(prices) {
+    fs.writeFileSync(PRICES_PATH, JSON.stringify(prices, null, 2));
+}
+
+function getPrice(name) {
+    const prices = loadPrices();
+    return prices[name] || null;
+}
+
+function setPrice(name, price) {
+    const prices = loadPrices();
+    prices[name] = price;
+    savePrices(prices);
+}
+
+
+module.exports = { loadList, saveList, loadPrices, savePrices, getPrice, setPrice };
+
 
